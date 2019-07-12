86cddcb20375433653c12801ef839354b7a9139d
retailbanktesta <retail.bank.test.a@gmail.com>
2019-07-12 13:59:14 +0800

    mod

--- test.sh
+++ test.sh
@@ -2,6 +2,12 @@
 
 # set environment configurations
 echo "hello world"
+echo "argument 1: $1"
+echo "argument 2: $2"
+echo "argument 3: $3"
+echo "argument 4: $4"
+echo "argument 5: $5"
+echo "argument 6: $6"
 
 cat > test.conf << EOF
 key="VALUE"
