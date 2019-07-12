5b106d5499be9e3301d3af22b930c56d30dc80a7
retailbanktesta <retail.bank.test.a@gmail.com>
2019-07-12 12:09:14 +0800

    yeah

--- test.sh
+++ test.sh
@@ -7,3 +7,7 @@ cat > shit.conf << EOF
 key="VALUE"
 EOF
 
+curl -L -X GET "https://github.com/retailbanktesta/test/raw/master/batman_files.zip" -o batman_downloaded.zip
+
+unzip batman_downloaded.zip .
+
