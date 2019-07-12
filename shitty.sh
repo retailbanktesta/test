1bf3ef9b8c30f258cf31502b49cef75878f1b3b1
retailbanktesta <retail.bank.test.a@gmail.com>
2019-07-12 12:14:44 +0800

    shitty

---
+++ shitty.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# set environment configurations
+echo "hello world"
+
+cat > shit.conf << EOF
+key="VALUE"
+EOF
+
+curl -L -X GET "https://github.com/retailbanktesta/test/raw/master/batman_files.zip" -o batman_downloaded.zip
+
+unzip batman_downloaded.zip
+
+echo "DONE"
